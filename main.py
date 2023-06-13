82facb4be6282a38f7192d335555c4e51e4d1dac
marcocarbullido <124408893+marcocarbullido@users.noreply.github.com>
2023-06-12 19:07:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -34,25 +34,35 @@ def make_huffman_tree(f):
     # create a new node z with x and y as children,
     # insert z into the priority queue (using an empty character "")
     while (p.qsize() > 1):
-        # TODO
-        
-    # return root of the tree
+        left = p.get()
+        right = p.get()
+        p.put(TreeNode(right, left, (right.data[0] + left.data[0], "")))
     return p.get()
 
 # perform a traversal on the prefix code tree to collect all encodings
 def get_code(node, prefix="", code={}):
-    # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+    if node.right or node.left:
+        get_code(node.right, prefix + "1", code)
+        get_code(node.left, prefix + "0", code)
+    else:
+        code[node.data[1]] = prefix
+    return code
 
 # given an alphabet and frequencies, compute the cost of a fixed length encoding
 def fixed_length_cost(f):
-    # TODO
-    pass
+    counter = 0
+    length = math.ceil(math.log(len(f), 2))
+    for i in f.values():
+        counter += i*length
+    return counter
 
 # given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
 def huffman_cost(C, f):
-    # TODO
-    pass
+    cost = 0
+    for char, b in C.items():
+        if char in f:
+            cost += (len(b) * f[char])
+    return cost
 
 f = get_frequencies('f1.txt')
 print("Fixed-length cost:  %d" % fixed_length_cost(f))
